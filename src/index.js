599353baaa2f6b0931decdbcbe876866a1139d1a
Alexrajdhami <alexrajdhami611@gmail.com>
2024-12-11 23:49:23 +0000

    Week-9 Lab
    Signed-off-by: Alexrajdhami <alexrajdhami611@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Hook_ControlledButtonState from './counter';
-import EmojeeCounter from './Emojicounter';
-import TextboxLabelComponent from './TextboxLabelComponent';
+import reportWebVitals from './reportWebVitals';
+import FacebookEmojiCounter from './FacebookEmoji';
+import ToggleMode from './ToggleModeComponent';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <FacebookEmojiCounter  type= "like"/>
+     < FacebookEmojiCounter type= "Love"/> 
+     < FacebookEmojiCounter type= "happy"/> 
+     < ToggleMode/> 
     <App />
-    <Hook_ControlledButtonState />
-    <EmojeeCounter pic='Love'/> 
-<EmojeeCounter pic='sad'/> 
-<EmojeeCounter pic='Like'/> 
-<TextboxLabelComponent /> {/* New Component Added */}
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
