b2b1d428f55088025d90beb675d8848c8663dc63
Alexrajdhami <alexrajdhami611@gmail.com>
2024-12-11 15:00:43 +0000

    Add files via upload
    Signed-off-by: Alexrajdhami <alexrajdhami611@gmail.com>

---
+++ week-1/example-4.js
@@ -0,0 +1,11 @@
+//enter the number
+const a = 10;
+const b = 5;
+//add two numbers
+console.log("Addition: " + (a + b));
+//subtract two numbers
+console.log("Subtraction: " + (a - b));
+//multiply two numbers
+console.log("Multiplication: " + (a * b));
+//divide two numbers
+console.log("Division: " + (a / b));
